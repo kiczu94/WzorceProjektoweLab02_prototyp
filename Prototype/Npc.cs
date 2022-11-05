1df27303a91fc01bcb9184b9cce93bc5dee1842b
Tomasz Tkocz <tkoczt@gmail.com>
2022-11-05 15:54:03 +0100

    Prototype

---
+++ Prototype/Npc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Npc : ICharacter
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        private Npc(Npc npc)
+        {
+            Id = Guid.NewGuid();
+            Name = npc.Name;
+            Gender = npc.Gender;
+            Age = npc.Age;
+        }
+
+        public Npc(string name, string gender, int age)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Gender = gender;
+            Age = age;
+        }
+
+        public ICharacter Clone()
+        {
+            var copy = new Npc(this);
+            return copy;
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Npc("Janusz", "Male", 24);
+            var person2 = (Npc)person.Clone();
+
+            Console.WriteLine($"First person data: name:{person.Name} , gender: {person.Gender}, Age: {person.Age}, id: {person.Id}");
+            Console.WriteLine($"Second person data: name:{person2.Name} , gender: {person2.Gender}, Age: {person2.Age}, id: {person2.Id}");
+        }
+    }
+}
